be1044ccf791b02f89cf3fcdca15a96c290a647c
CM-Duncan <carolineduncan51@gmail.com>
2026-02-28 04:10:39 +0000

    rest api framework done

---
+++ homework2/movie_theater_booking/bookings/serializers.py
@@ -0,0 +1,18 @@
+"Caroline Duncan, 2/27/26, converts the model objects to/from JSON "
+from rest_framework import serializers
+from .models import Movie, Seat, Booking
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+class SeatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seat
+        fields = '__all__'
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
