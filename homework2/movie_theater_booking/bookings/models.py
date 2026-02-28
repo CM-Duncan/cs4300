25f16b8a1db15f86e80b9d0231fdb8825e4edd76
CM-Duncan <carolineduncan51@gmail.com>
2026-02-28 03:04:14 +0000

    integrated all three model files in the right place this time

--- homework2/movie_theater_booking/bookings/models.py
+++ homework2/movie_theater_booking/bookings/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+"Caroline Duncan, 2/27/26, all three files: Models,seat_model, booking_model combined "
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration = models.IntegerField(help_text="Time in minutes")
+
+    def __str__(self):
+        return self.title
+
+class Seat(models.Model):
+    seat_number = models.CharField(max_length=10)
+    is_booked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Seat: {self.seat_number}"
+
+class Booking(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.movie} - {self.seat}"
