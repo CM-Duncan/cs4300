17909e3fbc162c99892b0803634bea0e41f37134
CM-Duncan <carolineduncan51@gmail.com>
2026-02-28 03:13:53 +0000

    inital files were placed in wrong folder so they were removed, everything is in the right place

--- homework2/movie_theater_booking/seat_model.py
+++
@@ -1,7 +0,0 @@
-"Caroline Duncan, 2/27/26, each seat has a number and whether it's booked or not "
-class Seat(models.Model):
-    seat_number = models.CharField(max_length=10)
-    is_booked = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"Seat {self.seat_number}"â
