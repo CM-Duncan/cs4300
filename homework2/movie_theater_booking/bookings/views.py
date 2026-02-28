48022c228dc63c2dd43a8308d20250fa07611b2b
CM-Duncan <carolineduncan51@gmail.com>
2026-02-28 15:51:34 +0000

    views.py done

--- homework2/movie_theater_booking/bookings/views.py
+++ homework2/movie_theater_booking/bookings/views.py
@@ -1,3 +1,28 @@
+"Caroline Duncan, 2/28/26, sets up a rest api endpoint that lets authenticated-or-open users perform full CRUD operations on movie objects "
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Movie, Seat, Booking
+from .serializers import MovieSerializer, SeatSerializer, BookingSerializer
 
-# Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Only return the current user's bookings
+        return Booking.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer): 
+        seat = serializer.validated_data['seat']
+        seat.is_booked = True
+        seat.save()
+        serializer.save(user=self.request.user)
